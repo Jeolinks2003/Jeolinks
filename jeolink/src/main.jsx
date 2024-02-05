8d7cd3927290bb81b191c992aa25a37eb51aa5db
Jeolinks <153494354+Jeolinks@users.noreply.github.com>
2024-02-05 15:54:17 +0000

    nothing

--- jeolink/src/main.jsx
+++ jeolink/src/main.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { Route, Router, RouterProvider, Routes,} from "react-router-dom";
+import { BrowserRouter as Router,Route, Routes,} from "react-router-dom";
 import './App.css'
-import App from './App';
+import App from "./App"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
     <Routes>
-    <Route path='/*' element={<App/>}/>
+    <Route path="/*" element={<App/>}/>
     </Routes>
     </Router>
   </React.StrictMode>
